dfe6f0d76e05e73706eea98074034f2ebd5ef939
alvant01 <alvant01@ucm.es>
2018-06-12 17:48:03 +0200

    Add files via upload

---
+++ javascript/jsFollow.js
@@ -0,0 +1,30 @@
+/*follow, follower
+    function seguir()
+    {
+		echo "corrercto";
+            if(Seguidos::alredyFollow($follow, $follower))
+            {
+                Seguidos::unFollow($follow, $follower);
+                return false;
+            }
+            else
+            {
+                Seguidos::follow($follow, $follower);
+                return true;
+            }
+    }
+    $(#Follow).click(function(){
+    if ( seguir($("#follow").val(), $("#follower").val()) ) {
+        $("#UrlFollow").hide();
+        $("#UrlUnfollow").show();
+    } else {
+        $("#UrlFollow").show();
+        $("#UrlUnfollow").hide();
+    }
+});*/
+
+function seguir(follow, follower, seguir)
+{
+    echo "hola";
+    
+}
